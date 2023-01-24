ae4dcecda7ffcde282e4345b4a222c6bddd72ac7
Bereket <bereketzewdu15@gmail.com>
2023-01-24 06:12:03 -0800

    Group project printf

---
+++ functions2.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_pointer - Prints the value of a pointer variable
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed.
+ */
+
